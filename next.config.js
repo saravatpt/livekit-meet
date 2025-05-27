6dbf8edf35bf22c9333501b735fac3e314ab4212
Saravanan Narayanan <saravatpt@gmail.com>
2025-05-27 06:56:45 +0530

    Initial Commit

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  productionBrowserSourceMaps: true,
+  images: {
+    formats: ['image/webp'],
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+    // Important: return the modified config
+    config.module.rules.push({
+      test: /\.mjs$/,
+      enforce: 'pre',
+      use: ['source-map-loader'],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
